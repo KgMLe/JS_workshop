8d091b63832339410f1347ca4c00db3dc0d4d0d2
KgMLe <kgodisholebopa@gmail.com>
2023-05-17 10:27:08 +0200

    JsDay3

---
+++ script/code.js
@@ -0,0 +1,14 @@
+let myname = 'Kgodisho'
+console.log(myname);
+console.log(typeof myname);
+myname= 1999
+console.log(myname);
+console.log (typeof myname)
+myname = false
+console.log(myname);
+console.log(typeof myname);
+myname;
+console.log(myname);
+myname = null
+console.log(typeof myname);
+
